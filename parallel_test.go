ecbb48f80f05e23e43fec886e670038822f15c0e
zhouhongyu <zhouhongyu@shihuituan.com>
2021-06-29 20:40:55 +0800

    1

---
+++ parallel_test.go
@@ -0,0 +1,31 @@
+package util_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/dashessmith/util"
+)
+
+func Test_Parall(t *testing.T) {
+	N := 1000000000
+	search := 100
+	arr := make([]int, N)
+	for i := 0; i < N; i++ {
+		arr[i] = rand.Intn(N)
+	}
+	d1 := util.Elapse(func() {
+		for _, n := range arr {
+			if n == search {
+				return
+			}
+		}
+	})
+	d2 := util.Elapse(func() {
+		res := util.AnyOf_P(N, func(i int) bool {
+			return arr[i] == search
+		})
+		t.Logf(" yes ? %v\n ", res)
+	})
+	t.Logf("d1 = %v, d2 = %v\n", d1, d2)
+}
