dcc4688e28c592b44fd7c67c784deb64ccc595b2
root <root@DESKTOP-RE0Q19V.localdomain>
2021-07-03 16:38:58 +0800

    1

--- json.go
+++ json.go
@@ -2,8 +2,12 @@ package util
 
 import "encoding/json"
 
-func JsonString(obj interface{}) string {
-	//bs, _ := json.Marshal(obj)
+func JsonStringPretty(obj interface{}) string {
 	bs, _ := json.MarshalIndent(obj, "", "    ")
+	return string(bs)
+}
+
+func JsonString(obj interface{}) string {
+	bs, _ := json.Marshal(obj)
 	return string(bs)
 }
