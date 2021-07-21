53b57eb532961a65e307e398074b94927b3deb27
dashessmith <dashessmith@gmail.com>
2021-07-21 11:01:02 +0800

    1

--- ulid.go
+++ ulid.go
@@ -1,27 +1,35 @@
 package util
 
 import (
-	"io"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/oklog/ulid/v2"
 )
 
-var _entropy io.Reader
+var chid chan string = make(chan string, 1024)
 
 func init() {
-	_entropy = ulid.Monotonic(rand.New(rand.NewSource(int64(ulid.Timestamp(time.Now())))), 0)
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(int64(ulid.Timestamp(time.Now())))), 0)
+	var lastid string
+	go func() {
+		for {
+			u, err := ulid.New(ulid.Timestamp(time.Now()), entropy)
+			if err != nil {
+				continue
+			}
+			now := time.Now()
+			id := strconv.FormatInt(now.UnixNano(), 10) + "_" + u.String()
+			if strings.Compare(id, lastid) > 0 {
+				chid <- id
+				lastid = id
+			}
+		}
+	}()
 }
 
 func ULID() string {
-	for {
-		u, err := ulid.New(ulid.Timestamp(time.Now()), _entropy)
-		if err != nil {
-			continue
-		}
-		now := time.Now()
-		return strconv.FormatInt(now.UnixNano(), 10) + "_" + u.String()
-	}
+	return <-chid
 }
