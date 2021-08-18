67f5d994ed9370217d3d2c91a3c55306c357e4eb
dashessmith <dashessmith@gmail.com>
2021-08-18 14:30:37 +0800

    ''

--- push.py
+++ push.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 from time import gmtime, strftime
 
 
@@ -10,13 +11,18 @@ def exec(cmd):
 
 
 def push():
+    maxtag = subprocess.check_output(
+        "git describe --abbrev=0 --tags").decode('utf8').strip()
+    tokens = maxtag.split('.')
+    lasttoken = int(tokens[-1]) + 1
+    newtag = ".".join(tokens[:-1] + [str(lasttoken)])
     exec("go mod tidy")
     exec("gofumpt -s -w .")
     exec("git add .")
     exec("git commit --allow-empty-message -m ''")
     exec("git push")
     exec(
-        f'gh release create v{strftime("%Y.%m.%d.%H.%M.%S", gmtime())} --notes "casual release"')
+        f'gh release create {newtag} --notes "casual release"')
     exec("git pull --tags")
 
 
