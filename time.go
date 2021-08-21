7c3835904c403b345a3209fbbaf472346fcb07ba
dashessmith <dashessmith@gmail.com>
2021-08-21 18:44:13 +0800

    ''

--- time.go
+++ time.go
@@ -4,7 +4,7 @@ import "time"
 
 // 一日开始时间 2021-5-26 19:20 ==> 2021-5-26 00:00
 func TimeFloorDay(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, nil)
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
 }
 
 // 次日开始时间 2021-5-26 19:20 ==> 2021-5-27 00:00
@@ -14,7 +14,7 @@ func TimeCeilDay(t time.Time) time.Time {
 
 // 一周开始时间
 func TimeFloorWeek(t time.Time) time.Time {
-	res := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, nil)
+	res := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
 	switch wd := int(t.Weekday()); wd {
 	case 0: // 星期日
 		res = res.AddDate(0, 0, -6)
@@ -31,7 +31,7 @@ func TimeCeilWeek(t time.Time) time.Time {
 
 // 一月开始时间
 func TimeFloorMonth(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, nil)
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.Local)
 }
 
 // 下月开始时间
