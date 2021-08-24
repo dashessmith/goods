434f0364c5b2d03ff9a8fa42b2e5c549b179392d
dashessmith <dashessmith@gmail.com>
2021-08-25 01:35:51 +0800

    dberror

--- dberror.go
+++ dberror.go
@@ -1,6 +1,8 @@
 package goods
 
 import (
+	"strings"
+
 	"github.com/lib/pq"
 )
 
@@ -15,6 +17,10 @@ func IsViolatesUniqueError(err error, columns ...string) bool {
 				return true
 			}
 		}
+	}
+	// ERROR: duplicate key value violates unique constraint "idx_t_users_token" (SQLSTATE 23505)
+	if len(columns) <= 0 && strings.Contains(err.Error(), "ERROR: duplicate key value violates unique constraint") {
+		return true
 	}
 	return false
 }
