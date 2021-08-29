1cc30d311d1158dd04829530656a615bb2711aae
dashessmith <dashessmith@gmail.com>
2021-08-29 11:16:15 +0800

    timed cache

--- timed_cache.go
+++ timed_cache.go
@@ -57,10 +57,58 @@ func (tc *Timedcache) AsyncGet(key string, asyncDefault interface{}, fetchfunc f
 			return item.x, nil
 		}
 	} else {
-		actual, _ = tc.data.LoadOrStore(key, &timedcacheitem{
-			x: asyncDefault,
-		})
+		actual, _ = tc.data.LoadOrStore(key, &timedcacheitem{})
 		item = actual.(*timedcacheitem)
+		WithMutex(&item.mtx, func() {
+			if item.x == nil {
+				item.x = asyncDefault
+			}
+		})
+	}
+	x = item.x
+	if item.fetching {
+		return
+	}
+	WithMutex(&item.mtx, func() {
+		if item.et.After(time.Now()) {
+			return
+		}
+		if item.fetching {
+			return
+		}
+		item.fetching = true
+		go func() {
+			defer func() {
+				item.fetching = false
+			}()
+			var kt time.Duration
+			x, kt, err = fetchfunc()
+			if err != nil {
+				return
+			}
+			item.x = x
+			item.et = time.Now().Add(kt)
+		}()
+	})
+	return
+}
+
+func (tc *Timedcache) AsyncGetf(key string, f func() interface{}, fetchfunc func() (x interface{}, kt time.Duration, err error)) (x interface{}, err error) {
+	var item *timedcacheitem
+	actual, _ := tc.data.Load(key)
+	if actual != nil {
+		item = actual.(*timedcacheitem)
+		if item.et.After(time.Now()) {
+			return item.x, nil
+		}
+	} else {
+		actual, _ = tc.data.LoadOrStore(key, &timedcacheitem{})
+		item = actual.(*timedcacheitem)
+		WithMutex(&item.mtx, func() {
+			if item.x == nil {
+				item.x = f()
+			}
+		})
 	}
 	x = item.x
 	if item.fetching {
