bfd19f13833a968922292c8e607dad44ce8b9003
dashessmith <dashessmith@gmail.com>
2021-08-29 10:49:15 +0800

    xy meter dis

--- xy_test.go
+++ xy_test.go
@@ -3,6 +3,11 @@ package goods
 import "testing"
 
 func TestXYDistance(t *testing.T) {
-	dis := XYDistance(112.937748, 28.223977, 112.937748, 28.223977)
+	dis := XYDistance(112.937748, 28.223977, 112.936748, 28.223977)
+	t.Logf("%v\n", dis)
+}
+
+func TestXYDistanceM(t *testing.T) {
+	dis := XYDistanceMeter(112.937748, 28.223977, 112.936748, 28.223977)
 	t.Logf("%v\n", dis)
 }
