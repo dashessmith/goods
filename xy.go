4a7e1dacf90119761a6adbe7e4d17b7fc13b1f83
smith <dashessmith@gmail.com>
2023-06-14 14:36:59 +0800

    ''

--- xy.go
+++ xy.go
@@ -15,6 +15,7 @@ func XYDistance(x1 float64, y1 float64, x2 float64, y2 float64) (dis float64) {
 // in meter
 // x
 // y
+// z
 func XYDistanceMeter(x1 float64, y1 float64, x2 float64, y2 float64) (dis int) {
 	return RoundFloat64(XYDistance(x1, y1, x2, y2))
 }
